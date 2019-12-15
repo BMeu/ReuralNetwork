7de806d1bfc85bb0d5c94a4655b16ef474a647f4
Bastian Meyer <bastian@bastianmeyer.eu>
2019-12-15 14:12:49 +0100

    Add a copyright header

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,10 @@
+// Copyright 2019 Bastian Meyer
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT o
+// http://opensource.org/licenses/MIT>, at your option. This file may not be copied, modified, or
+// distributed except according to those terms.
+
 fn main() {
     println!("{}", greet());
 }
