c88998edc0db0aa8cea9ba2a50207c93a38cf0e9
Bastian Meyer <bastian@bastianmeyer.eu>
2019-12-27 15:10:54 +0100

    Improve module documentation

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be copied, modified, or
 // distributed except according to those terms.
 
-//! Error handling.
+//! Error handling and related stuff.
 
 use std::error;
 use std::fmt;
